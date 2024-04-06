0072b57e1e4e55d8cdd95b75d613aae02eaad69a
Chimwemwe Masona <justplainrodney@gmail.com>
2024-04-06 12:02:45 +0200

    added deposit page

--- frontend/src/pages/deposit.js
+++ frontend/src/pages/deposit.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import Link from "next/link";
+import withdraw from "./withdraw";
 
 const Deposit = () => {
+  const [deposit, setDeposit] = useState({
+    amount: "",
+  });
+
+  const handleDepositChange = (e) => {
+    const { name, value } = e.target;
+
+    setDeposit({
+      ...deposit,
+      [name]: value,
+    });
+  };
+
+  const handleDepositSubmit = (e) => {
+    e.preventDefault();
+    console.log(withdraw);
+
+    const sendData = async () => {
+      const response = await fetch(`/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(withdraw),
+      });
+      if (response.ok) {
+        alert("Deposited successfully");
+      }
+
+      if (response.status === 400) {
+        console.log("Error:", response.status);
+      }
+    };
+
+    //   sendData();
+  };
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form>
+        <div>
+          <label htmlFor="deposit">Deposit</label>
 
-export default Deposit
+          <input
+            type="text"
+            name="amount"
+            id="deposit_amount"
+            value={deposit.amount}
+            onChange={handleDepositChange}
+          />
+        </div>
+
+        <button onClick={handleDepositSubmit}>Submit</button>
+      </form>
+      <Link href={"/"}>Home</Link>
+    </div>
+  );
+};
+
+export default Deposit;
